937eb680e9d5d27a6d54701c0143e943fd8fc630
avelreserch <pavel.oliynyk@gmail.com>
2019-07-17 17:02:08 +0100

    AkkaHttp testing

--- src/test/scala/http/GenericSpec.scala
+++ src/test/scala/http/GenericSpec.scala
@@ -4,22 +4,29 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.avel.wayoftesting.http.{Book, BookJsonProtocol}
 import org.scalatest.{Matchers, WordSpec}
+import com.avel.wayoftesting.http.Generic._
 
 
 class GenericSpec extends WordSpec with Matchers with ScalatestRouteTest with BookJsonProtocol {
 
-  import com.avel.wayoftesting.http.Generic._
+  type Books = List[Book]
 
   "A digital library backend" should {
+
     "return all the books in the library" in {
-      // send an HTTP request through an endpoint that you want to test
-      // inspect the response
       Get("/api/book") ~> libraryRoute ~> check {
-        // assertions
         status shouldBe StatusCodes.OK
-        entityAs[List[Book]] shouldBe books
+        entityAs[Books] shouldBe books
       }
     }
+
+    "return a book by hitting the query parameter endpoint" in {
+      Get("/api/book?id=2") ~> libraryRoute ~> check {
+        status shouldBe StatusCodes.OK
+        responseAs[Option[Book]] shouldBe Some(Book(2, "JRR Tolkien", "The Lord of the Rings"))
+      }
+    }
+
 
   }
 
