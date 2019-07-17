6134a48fe42166822879c10b84c273534a7921ed
avelreserch <pavel.oliynyk@gmail.com>
2019-07-17 11:25:28 +0100

    Update Akka streams unit test

--- src/main/scala/com/avel/wayoftesting/streams/Akka.scala
+++ src/main/scala/com/avel/wayoftesting/streams/Akka.scala
@@ -2,15 +2,28 @@ package com.avel.wayoftesting.streams
 
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
+import scala.util.Random
+
 object Akka {
 
   def simpleSource(n: Int) = Source(1 to n)
 
   def funSource(n: Int, f: Int => Int) = Source(1 to n).map(f)
 
+  def faultySink(n: Int) = {
+    //val failure = Random.nextInt(n)
+    Sink.foreach[Int] {
+      case 13 =>
+        throw new RuntimeException(s"bad luck on")
+      case _ =>
+    }
+  }
+
   def simpleSink(z: Int) =
     Sink.fold(0)((a: Int, b: Int) => a + b)
 
   def simpleFlow() =
     Flow[Int].scan[Int](0)(_ + _)
+
+  def multiFlow() = Flow[Int].map(_ * 2)
 }
--- src/test/scala/streams/AkkaStreamSpec.scala
+++ src/test/scala/streams/AkkaStreamSpec.scala
@@ -1,9 +1,10 @@
 package streams
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
-import akka.stream.testkit.scaladsl.TestSink
+import akka.stream.testkit.scaladsl.{TestSink, TestSource}
 import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
@@ -11,6 +12,8 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import com.avel.wayoftesting.streams.Akka._
+
+import scala.util.{Failure, Success}
 
 
 class AkkaStreamSpec extends TestKit(ActorSystem("TestingAkkaStreams"))
@@ -77,6 +80,52 @@ class AkkaStreamSpec extends TestKit(ActorSystem("TestingAkkaStreams"))
       .request(5)
       .expectNext(3, 6, 9, 12, 15)
       .expectComplete()
+  }
+
+  "integrate with Streams TestKit Source" in {
+    import system.dispatcher
+
+    val sinkUnderTest = faultySink(15)
+
+    val testSource = TestSource.probe[Int]
+    val (testPublisher, resultFuture) = testSource
+      .toMat(sinkUnderTest)(Keep.both).run()
+
+    testPublisher
+      .sendNext(1)
+      .sendNext(5)
+      .sendNext(13)
+      .sendComplete()
+
+    resultFuture.onComplete {
+      case Success(_) =>
+        fail("the sink under test should have thrown an exception on 13")
+      case Failure(_) =>
+    }
+
+  }
+
+  "test flows with a test source AND a test sink" in {
+    val flowUnderTest = multiFlow()
+
+    val testSource = TestSource.probe[Int]
+    val testSink = TestSink.probe[Int]
+
+    val materialized = testSource.via(flowUnderTest).toMat(testSink)(Keep.both).run()
+    val (publisher, subscriber) = materialized
+
+    publisher
+      .sendNext(1)
+      .sendNext(5)
+      .sendNext(42)
+      .sendNext(99)
+      .sendComplete()
+
+    subscriber
+      .request(4) // don't forget this!
+      .expectNext(2, 10, 84, 198)
+      .expectComplete()
+
   }
 
 }
