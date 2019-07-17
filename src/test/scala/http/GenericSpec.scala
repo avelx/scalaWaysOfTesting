19796d680af9b6a1bd749db757e6456ac18b0274
avelreserch <pavel.oliynyk@gmail.com>
2019-07-17 18:03:27 +0100

    Update AkkaHttp unit tests

--- src/test/scala/http/GenericSpec.scala
+++ src/test/scala/http/GenericSpec.scala
@@ -1,10 +1,15 @@
 package http
 
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
+import akka.http.scaladsl.server.MethodRejection
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.avel.wayoftesting.http.{Book, BookJsonProtocol}
 import org.scalatest.{Matchers, WordSpec}
 import com.avel.wayoftesting.http.Generic._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import spray.json._
 
 
 class GenericSpec extends WordSpec with Matchers with ScalatestRouteTest with BookJsonProtocol {
@@ -24,6 +29,49 @@ class GenericSpec extends WordSpec with Matchers with ScalatestRouteTest with Bo
       Get("/api/book?id=2") ~> libraryRoute ~> check {
         status shouldBe StatusCodes.OK
         responseAs[Option[Book]] shouldBe Some(Book(2, "JRR Tolkien", "The Lord of the Rings"))
+      }
+    }
+
+    "return a book by calling the endpoint with the id in the path" in {
+      Get("/api/book/2") ~> libraryRoute ~> check {
+        response.status shouldBe StatusCodes.OK
+
+        val strictEntityFuture = response.entity.toStrict(1 second)
+        val strictEntity = Await.result(strictEntityFuture, 1 second)
+
+        strictEntity.contentType shouldBe ContentTypes.`application/json`
+
+        val book = strictEntity.data.utf8String.parseJson.convertTo[Option[Book]]
+        book shouldBe Some(Book(2, "JRR Tolkien", "The Lord of the Rings"))
+      }
+    }
+
+    "insert a book into the 'database'" in {
+      val newBook = Book(5, "Steven Pressfield", "The War of Art")
+      Post("/api/book", newBook) ~> libraryRoute ~> check {
+        status shouldBe StatusCodes.OK
+        assert(books.contains(newBook))
+        books should contain(newBook) // same
+      }
+    }
+
+    "not accept other methods than POST and GET" in {
+      Delete("/api/book") ~> libraryRoute ~> check {
+        rejections should not be empty   // "natural language" style
+        rejections.should(not).be(empty) // same
+
+        val methodRejections = rejections.collect {
+          case rejection: MethodRejection => rejection
+        }
+
+        methodRejections.length shouldBe 2
+      }
+    }
+
+    "return all the books of a given author" in {
+      Get("/api/book/author/JRR%20Tolkien") ~> libraryRoute ~> check {
+        status shouldBe StatusCodes.OK
+        entityAs[List[Book]] shouldBe books.filter(_.author == "JRR Tolkien")
       }
     }
 
