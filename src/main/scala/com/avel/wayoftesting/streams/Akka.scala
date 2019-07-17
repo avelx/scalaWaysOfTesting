31566c58debea55654c767d5024fd7e94c15cc9a
avelreserch <pavel.oliynyk@gmail.com>
2019-07-17 10:48:59 +0100

    Update Akka stream unit tests

--- src/main/scala/com/avel/wayoftesting/streams/Akka.scala
+++ src/main/scala/com/avel/wayoftesting/streams/Akka.scala
@@ -1,12 +1,16 @@
 package com.avel.wayoftesting.streams
 
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object Akka {
 
   def simpleSource(n: Int) = Source(1 to n)
 
-  def simpleSink(z: Int) = Sink.fold(0)((a: Int, b: Int) => a + b)
+  def funSource(n: Int, f: Int => Int) = Source(1 to n).map(f)
 
+  def simpleSink(z: Int) =
+    Sink.fold(0)((a: Int, b: Int) => a + b)
 
+  def simpleFlow() =
+    Flow[Int].scan[Int](0)(_ + _)
 }
--- src/test/scala/streams/AkkaStreamSpec.scala
+++ src/test/scala/streams/AkkaStreamSpec.scala
@@ -2,7 +2,8 @@ package streams
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
@@ -46,6 +47,36 @@ class AkkaStreamSpec extends TestKit(ActorSystem("TestingAkkaStreams"))
 
     probe
       .expectMsg(55)
+  }
+
+  "integrate with a test-actor-based sink" in {
+    val source = simpleSource(8)
+    val flow = simpleFlow()
+
+    val probe = TestProbe()
+    val probeSink = Sink
+      .actorRef(probe.ref, "completionMessage")
+
+    val streamUnderTest = source.via(flow)
+
+    streamUnderTest
+      .to(probeSink).run()
+
+    probe
+      .expectMsgAllOf(0, 1, 3, 6, 10, 15)
+  }
+
+  "integrate with Streams TestKit Sink" in {
+    val sourceUnderTest = funSource(5, x => x * 3)
+
+    val testSink = TestSink.probe[Int]
+    val materializedTestValue = sourceUnderTest
+      .runWith(testSink)
+
+    materializedTestValue
+      .request(5)
+      .expectNext(3, 6, 9, 12, 15)
+      .expectComplete()
   }
 
 }
